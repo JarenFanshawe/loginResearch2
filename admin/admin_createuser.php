0903ca41636830bdf8e73c2098f22111e8d99314
jarenfanshawe <j_mcdonald24@fanshaweonline.ca>
2018-02-23 15:12:43 -0500

    whoops, almos forgot to add the new db

--- admin/admin_createuser.php
+++ admin/admin_createuser.php
@@ -14,20 +14,20 @@
 
 	}
 
-	// if(isset($_POST['submit'])) {
-	// 	$fname = trim($_POST['fname']);
-	// 	$username = trim($_POST['username']);
-	// 	$password = trim($_POST['password']);
-	// 	$email = trim($_POST['email']);
-	// 	$userlvl = $_POST['userlvl'];
-    //
-	// 	if(empty($userlvl)){
-	// 		$message = "Please select a user level.";
-	// 	}else{
-	// 		$result = createUser($fname, $username, $password, $email, $userlvl);
-	// 		$message = $result;
-	// 	}
-	// }
+	if(isset($_POST['submit'])) {
+		$fname = trim($_POST['fname']);
+		$username = trim($_POST['username']);
+		$password = trim($_POST['password']);
+		$email = trim($_POST['email']);
+		$userlvl = $_POST['userlvl'];
+
+		if(empty($userlvl)){
+			$message = "Please select a user level.";
+		}else{
+			$result = createUser($fname, $username, $password, $email, $userlvl);
+			$message = $result;
+		}
+	}
 
 ?>
 <!doctype html>
