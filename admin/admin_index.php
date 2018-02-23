a07afcc2ae755bd001ff121d88e91b8813f93250
jarenfanshawe <j_mcdonald24@fanshaweonline.ca>
2018-02-23 11:19:32 -0500

    added generate random pass

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -8,14 +8,23 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>CMS Portal</title>
+	<title>CMS Admin Portal</title>
+
+	<link rel="stylesheet" href="css/foundation.css" />
+	<link rel="stylesheet" href="css/main.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Muli|Open+Sans" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
-	<h1><?php echo "{$_SESSION['user_name']}'s admin page"; ?></h1>
+	<h1 id="adminTitle"><?php echo "{$_SESSION['user_name']}'s admin page"; ?></h1>
 
-
-	<a href="admin_createuser.php">Create User</a><br>
-	<a href="admin_edituser.php">Edit User</a><br>
-	<a href="phpscripts/caller.php?caller_id=logout">Sign Out</a>
+	<section class="row fullWidth">
+		<ul id="adminNav">
+			<li><a href="admin_createuser.php">Create User</a></li>
+			<li><a href="admin_edituser.php">Edit User</a></li>
+			<li><a href="phpscripts/caller.php?caller_id=logout">Sign Out</a></li>
+		</ul>
+	</section>
 </body>
 </html>
--- admin/phpscripts/user.php
+++ admin/phpscripts/user.php
@@ -1,14 +1,18 @@
 <?php
-	
+
 	function createUser($fname, $username, $password, $email, $userlvl) {
 		include('connect.php');
-		$userString = "INSERT INTO tbl_user VALUES(NULL,'{$fname}', '{$username}', '{$password}', '{$email}', NULL, '{$userlvl}', 'no')";
+		include('config.php');
+
+		$userString = "INSERT INTO tbl_user VALUES(NULL,'{$fname}', '{$username}', '{$password}', '{$email}', NULL, 'no', '{$userlvl}')";
 		//echo $userString;
 		$userQuery = mysqli_query($link, $userString);
 		if($userQuery) {
+			mail($email, "New user account created!", "From: estyn72@gmail.com");
+
 			redirect_to("admin_index.php");
 		}else{
-			$message = "There was a problem setting up this user.  Maybe reconsider your hiring practices.";
+			$message = "There was a problem setting up this user. Maybe reconsider your hiring practices.";
 			return $message;
 		}
 		mysqli_close($link);
